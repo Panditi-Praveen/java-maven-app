342b286b45ba06441f65f07fe92372463053281f
Panditi-Praveen <panditipraveen467@gmail.com>
2026-06-23 16:12:21 +0530

    feat: maven added

--- src/main/java/com/java/maven/model/Customer.java
+++ src/main/java/com/java/maven/model/Customer.java
@@ -1,4 +1,17 @@
 package com.java.maven.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Builder
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    private int id;
+    private String name;
+    private String email;
+
 }
