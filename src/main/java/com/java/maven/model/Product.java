4b864a95225d0860771d97a40fa068b1ae6218f1
Panditi-Praveen <panditipraveen467@gmail.com>
2026-06-23 16:12:04 +0530

    feat: maven added

--- src/main/java/com/java/maven/model/Product.java
+++ src/main/java/com/java/maven/model/Product.java
@@ -1,4 +1,21 @@
 package com.java.maven.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Product {
+
+    private String name;
+    private String category;
+    private String company;
+    private int ratings;
 }
+
